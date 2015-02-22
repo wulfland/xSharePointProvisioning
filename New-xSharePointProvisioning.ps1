5d70b8b4dc06b572130665eee9a45cde40e82bc5
wulfland <wulfland@hotmail.com>
2015-02-22 04:48:38 -0800

    Added simple resource to provision fields.

--- New-xSharePointProvisioning.ps1
+++ New-xSharePointProvisioning.ps1
@@ -15,8 +15,21 @@ New-xDscResource -Name ALIS_xList -FriendlyName xList -ModuleName xSharePointPro
 
 Copy-Item .\DSCResources\ALIS_xList.psm1 "$modulePath\xSharePointProvisioning\DSCResources\ALIS_xList\ALIS_xList.psm1" -force
 
-Get-DscResource -Name xList 
+#Get-DscResource -Name xList 
+#Test-xDscResource -Name xList -Verbose
 
-Test-xDscResource -Name xList -Verbose
+# Create Field Resource
+$FieldXml    = New-xDscResourceProperty -Name FieldXml        -Type String       -Attribute Key -Description "The xml that represents the field."
+$Url         = New-xDscResourceProperty -Name Url             -Type String       -Attribute Required   -Description "The full url to the list."
+$Ensure      = New-xDscResourceProperty -Name Ensure          -Type String       -Attribute Write -Description "Set this to 'Present' to ensure that the field is present. Set it to 'Absent' to ensure that the field is deleted. Default: 'Present'." -ValidateSet @("Present", "Absent")
+$credential  = New-xDscResourceProperty -Name Credentials     -Type PSCredential -Attribute Write -Description "The credentials to use to login to the site."
 
-copy-item .\xSharePointProvisioning.psd1 "$modulePath\xSharePointProvisioning\xSharePointProvisioning.psd1"
+New-xDscResource -Name ALIS_xField -FriendlyName xField -ModuleName xSharePointProvisioning -Property @($FieldXml, $Url, $Ensure, $credential) -Path $modulePath
+
+Copy-Item .\DSCResources\ALIS_xField.psm1 "$modulePath\xSharePointProvisioning\DSCResources\ALIS_xField\ALIS_xField.psm1" -force
+
+#Get-DscResource -Name xField
+#Test-xDscResource -Name xField -Verbose
+
+# Override module
+copy-item .\xSharePointProvisioning.psd1 "$modulePath\xSharePointProvisioning\xSharePointProvisioning.psd1" -Force
---
+++ Test-FieldResource.ps1
@@ -0,0 +1,71 @@
+﻿if (-not $Url)
+{
+    $Url = Read-Host "Enter the url to the test site:"
+}
+
+if (-not $Credentials)
+{
+    $Credentials = Get-Credential
+}
+
+$ConfigurationData = @{
+    AllNodes = @(
+        @{
+            NodeName="*"
+            PSDscAllowPlainTextPassword=$true
+         }
+        @{
+            NodeName="localhost"
+         }
+    )
+}
+
+
+Configuration TestFieldResource
+{
+    param
+    (
+        [Parameter(Mandatory=$false)]
+        [PSCredential]$Credential,
+
+        [Parameter(Mandatory=$false)]
+        [string]$Url
+    )
+
+    Import-DscResource -ModuleName xSharePointProvisioning   -Name ALIS_xField
+    
+    Node $AllNodes.NodeName 
+    {
+        xField TextField
+        {
+            FieldXml = "<Field Type='Text' DisplayName='My Text Field' Name='MyTextField' />"
+            Url = "$Url/Lists/MyCustomList"
+            Ensure = "Present"
+            Credentials = $Credential
+        }
+
+        xField NoteField
+        {
+            FieldXml = "<Field Type='Note' DisplayName='My Note Field' Name='MyNoteField' />"
+            Url = "$Url/Lists/MyCustomList"
+            Ensure = "Present"
+            Credentials = $Credential
+        }
+
+        xField NumberField
+        {
+            FieldXml = "<Field Type='Number' DisplayName='My Number Field' Name='MyNumberField' />"
+            Url = "$Url/Lists/MyCustomList"
+            Ensure = "Present"
+            Credentials = $Credential
+        }
+    }
+}
+
+TestFieldResource -ConfigurationData $ConfigurationData -Url $Url -Credential $Credentials -verbose
+
+Restart-Service Winmgmt -force
+
+Start-DscConfiguration -Path .\TestFieldResource -Wait -Force -Verbose
+
+Get-DscConfiguration
