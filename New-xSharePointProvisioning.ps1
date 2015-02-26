bb6a98f8358b79c0749aea501c2c26d0784216b9
Kaufmann, Michael (Alegri) <michael_kaufmann@hugoboss.com>
2015-02-26 14:21:35 +0100

    Added resource for listitem.

--- New-xSharePointProvisioning.ps1
+++ New-xSharePointProvisioning.ps1
@@ -4,12 +4,12 @@
 Remove-Item -Path "$modulePath\xSharePointProvisioning" -Force -Recurse 
 
 # Create List Resource
-$Url         = New-xDscResourceProperty -Name Url             -Type String       -Attribute Key   -Description "The full url to the list."
-$Title       = New-xDscResourceProperty -Name Title           -Type String       -Attribute Write -Description "The desired title of the list."
-$Ensure      = New-xDscResourceProperty -Name Ensure          -Type String       -Attribute Write -Description "Set this to 'Present' to ensure that the list is present. Set it to 'Absent' to ensure that the list is deleted. Default: 'Present'." -ValidateSet @("Present", "Absent")
-$Description = New-xDscResourceProperty -Name Description     -Type String       -Attribute Write -Description "The desired description of the list."
-$TemplateId  = New-xDscResourceProperty -Name Template        -Type String       -Attribute Write -Description "The template for of the list (default: genericList)."
-$credential  = New-xDscResourceProperty -Name Credentials     -Type PSCredential -Attribute Write -Description "The credentials to use to login to the site."
+$Url         = New-xDscResourceProperty -Name Url             -Type String       -Attribute Key       -Description "The full url to the list."
+$Title       = New-xDscResourceProperty -Name Title           -Type String       -Attribute Write     -Description "The desired title of the list."
+$Ensure      = New-xDscResourceProperty -Name Ensure          -Type String       -Attribute Write     -Description "Set this to 'Present' to ensure that the list is present. Set it to 'Absent' to ensure that the list is deleted. Default: 'Present'." -ValidateSet @("Present", "Absent")
+$Description = New-xDscResourceProperty -Name Description     -Type String       -Attribute Write     -Description "The desired description of the list."
+$TemplateId  = New-xDscResourceProperty -Name Template        -Type String       -Attribute Write     -Description "The template for of the list (default: genericList)."
+$credential  = New-xDscResourceProperty -Name Credentials     -Type PSCredential -Attribute Write     -Description "The credentials to use to login to the site."
 
 New-xDscResource -Name ALIS_xList -FriendlyName xList -ModuleName xSharePointProvisioning -Property @($Url, $Ensure, $Title, $Description, $TemplateId, $credential) -Path $modulePath
 
@@ -19,10 +19,10 @@ Copy-Item .\DSCResources\ALIS_xList.psm1 "$modulePath\xSharePointProvisioning\DS
 #Test-xDscResource -Name xList -Verbose
 
 # Create Field Resource
-$FieldXml    = New-xDscResourceProperty -Name FieldXml        -Type String       -Attribute Key -Description "The xml that represents the field."
+$FieldXml    = New-xDscResourceProperty -Name FieldXml        -Type String       -Attribute Key        -Description "The xml that represents the field."
 $Url         = New-xDscResourceProperty -Name Url             -Type String       -Attribute Required   -Description "The full url to the list."
-$Ensure      = New-xDscResourceProperty -Name Ensure          -Type String       -Attribute Write -Description "Set this to 'Present' to ensure that the field is present. Set it to 'Absent' to ensure that the field is deleted. Default: 'Present'." -ValidateSet @("Present", "Absent")
-$credential  = New-xDscResourceProperty -Name Credentials     -Type PSCredential -Attribute Write -Description "The credentials to use to login to the site."
+$Ensure      = New-xDscResourceProperty -Name Ensure          -Type String       -Attribute Write      -Description "Set this to 'Present' to ensure that the field is present. Set it to 'Absent' to ensure that the field is deleted. Default: 'Present'." -ValidateSet @("Present", "Absent")
+$credential  = New-xDscResourceProperty -Name Credentials     -Type PSCredential -Attribute Write      -Description "The credentials to use to login to the site."
 
 New-xDscResource -Name ALIS_xField -FriendlyName xField -ModuleName xSharePointProvisioning -Property @($FieldXml, $Url, $Ensure, $credential) -Path $modulePath
 
@@ -30,6 +30,20 @@ Copy-Item .\DSCResources\ALIS_xField.psm1 "$modulePath\xSharePointProvisioning\D
 
 #Get-DscResource -Name xField
 #Test-xDscResource -Name xField -Verbose
+
+# Create ListItem Resource
+$Title       = New-xDscResourceProperty -Name Title           -Type String       -Attribute Key        -Description "The tiel of the listitem." 
+$Url         = New-xDscResourceProperty -Name Url             -Type String       -Attribute Required   -Description "The full url to the list."
+$Ensure      = New-xDscResourceProperty -Name Ensure          -Type String       -Attribute Write      -Description "Set this to 'Present' to ensure that the item is present. Set it to 'Absent' to ensure that the item is deleted. Default: 'Present'." -ValidateSet @("Present", "Absent")
+$Properties  = New-xDscResourceProperty -Name Properties      -Type Hashtable[]  -Attribute Write      -Description "The properties of the listitem."
+$credential  = New-xDscResourceProperty -Name Credentials     -Type PSCredential -Attribute Write      -Description "The credentials to use to login to the site."
+
+New-xDscResource -Name ALIS_xListItem -FriendlyName xListItem -ModuleName xSharePointProvisioning -Property @($Title, $Url, $Ensure, $Properties, $credential) -Path $modulePath
+
+Copy-Item .\DSCResources\ALIS_xListItem.psm1 "$modulePath\xSharePointProvisioning\DSCResources\ALIS_xListItem\ALIS_xListItem.psm1" -force
+
+#Get-DscResource -Name xListItem
+#Test-xDscResource -Name xListItem -Verbose
 
 # Override module
 copy-item .\xSharePointProvisioning.psd1 "$modulePath\xSharePointProvisioning\xSharePointProvisioning.psd1" -Force
---
+++ Test-ListItemResource.ps1
@@ -0,0 +1,59 @@
+﻿if (-not $Url)
+{
+    $Url = Read-Host "Enter the url to the test site:"
+}
+
+if (-not $Credentials)
+{
+    $Credentials = Get-Credential
+}
+
+$ConfigurationData = @{
+    AllNodes = @(
+        @{
+            NodeName="*"
+            PSDscAllowPlainTextPassword=$true
+         }
+        @{
+            NodeName="localhost"
+         }
+    )
+}
+
+
+Configuration TestListItemResource
+{
+    param
+    (
+        [Parameter(Mandatory=$false)]
+        [PSCredential]$Credential,
+
+        [Parameter(Mandatory=$false)]
+        [string]$Url
+    )
+
+    Import-DscResource -ModuleName xSharePointProvisioning   -Name ALIS_xListItem
+    
+    Node $AllNodes.NodeName 
+    {
+        xListItem TestItem
+        {
+            Url = "$Url/Lists/MyCustomList"
+            Ensure = "Present"
+            Title = "My Custom List"
+            Properties = @{ 
+                MyNumberField = '6' 
+                Date = '26/2/2015'
+                }
+            Credentials = $Credential
+        }
+    }
+}
+
+TestListItemResource -ConfigurationData $ConfigurationData -Url $Url -Credential $Credentials -verbose
+
+Restart-Service Winmgmt -force
+
+Start-DscConfiguration -Path .\TestListItemResource -Wait -Force -Verbose -debug
+
+#Get-DscConfiguration
